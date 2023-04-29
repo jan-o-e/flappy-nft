d7363953d7a3c1cb1b7bf33ff73defd43105281c
Kyle Duffy <44502053+kyduff@users.noreply.github.com>
2023-04-29 23:22:44 +0100

    Style things up

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,14 +1,18 @@
+import { ArrowForwardIcon } from "@chakra-ui/icons";
 import { Button } from "@chakra-ui/react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Link from "next/link";
 import { useEffect, useState } from "react"
 import { useAccount } from "wagmi";
 
 export default function Home() {
 
   const [score, setScore] = useState(0);
-  const {address, connector, isConnected} = useAccount();
+  const { address, connector, isConnected } = useAccount();
+  const [domLoaded, setDomLoaded] = useState(false);
 
   useEffect(() => {
+    setDomLoaded(true);
     let score = localStorage.getItem("FlappyBirdBestScore")
     if (score) {
       setScore(score);
@@ -20,22 +24,33 @@ export default function Home() {
   }
 
   return (
-    <div>
-      <div className="font-mono text-center text-[200px] pt-24">
-        {`${score}`}
-      </div>
-      <div className="font-mono text-center text-2xl pb-12">
-        Mint your prize!
-      </div>
-      <div className="flex justify-center">
-        {(isConnected) ? (
-          <Button onClick={handleMint}>
-            Mint!
-          </Button>
-        ) : (
-          <ConnectButton />
-        )}
-      </div>
-    </div>
+    <>
+      {domLoaded && (
+        <div>
+          <div className="font-mono text-center text-[200px] pt-24">
+            {`${score}`}
+          </div>
+          <div className="font-mono text-center text-2xl pb-12">
+            Mint your prize!
+          </div>
+          <div className="flex justify-center">
+            <div className="pb-12">
+              {(isConnected) ? (
+                <Button colorScheme="green" onClick={handleMint} size='lg'>
+                  Mint!
+                </Button>
+              ) : (
+                <ConnectButton />
+              )}
+            </div>
+          </div>
+          <div className="font-mono text-center text-2xl p-1 animate-bounce">
+            <Link className="hover:underline" href="/html/index.html">
+              Play again
+            </Link>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
